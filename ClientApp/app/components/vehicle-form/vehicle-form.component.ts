564ee736b0e125226d7ec7b6a808050adf3b10d7
ben <wbpiggot@gmail.com>
2018-01-31 15:24:14 -0800

    Refactoring services

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,4 +1,4 @@
-import { MakeService } from './../../services/make.service';
+import { VehicleService } from './../../services/vehicle.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,16 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleFormComponent implements OnInit {
   makes: any[];
+  features: any[];
   models: any[];
   vehicle: any = { };
 
-  constructor(private makeService: MakeService) { }
+  constructor(private vehicleService: VehicleService) { }
 
   ngOnInit() {
-    this.makeService.getMakes().subscribe(makes => this.makes = makes)
+    this.vehicleService.getMakes().subscribe(makes => this.makes = makes)
+    this.vehicleService.getFeatures().subscribe(features => this.features = features)
   }
   onMakeChange() {
-    debugger
     const selectedMake = this.makes.find(m => m.id === parseInt(this.vehicle.make))
     this.models = selectedMake ? selectedMake.models : [];
   }
--- ClientApp/app/services/vehicle.service.ts
+++ ClientApp/app/services/vehicle.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core'
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class MakeService {
+export class VehicleService {
 
   constructor(private http: Http) { }
 
@@ -12,4 +12,8 @@ export class MakeService {
       .map(res => res.json())
   }
 
+  getFeatures() {
+    return this.http.get('/api/features')
+      .map(res => res.json())
+  }
 }
